8378e9dc977f0177923302b4e774959e08c24f62
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-07-28 15:09:15 +0100

    Zipped

--- Batch Projects/Operating System7/Notepad.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Notepad.exe
-color f0
-:Document
-set /p input=Document:
-goto Document
--- Batch Projects/VirtualSand Box/maker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title file maker
-:a
-set/p name=enter name for your your file:
-set/p extension=enter the extension for your file:
-echo.>%name%.%extesion%
-goto :a
