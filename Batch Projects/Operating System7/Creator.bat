79f9a71b9d69dd0c472add2b4e904146cfecbd4d
Raiyaxi-ZIaoi <82474669+Raiyaxi-Ziaoi@users.noreply.github.com>
2022-07-30 16:03:14 +0100

    Deleted Some

--- Batch Projects/Operating System7/Creator.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title File creator
-:a
-cls
-set/p name=enter name for your your file:
-set/p extension=enter the extension for your file:
-echo.>"d:\Operating system7\Files created\%name%.%extension%"
-goto :a
